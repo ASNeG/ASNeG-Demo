1d72690e85310f8fa07cf626573c421b84fb6280
Kai Huebl <kai@huebl-sgh.de>
2017-09-13 20:35:26 +0200

    added windows build script

---
+++ build.bat
@@ -0,0 +1,13 @@
+
+rmdir ./build
+
+
+cmake.exe -G"Visual Studio 12 2013" -DOPCUASTACK_INSTALL_PREFIX=C:\install -H./src/ -B./build 
+
+
+
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe ./build/INSTALL.vcxproj
+
+
+
+pause
