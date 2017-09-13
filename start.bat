0e3692e1b609a09b170cbd6abbdb577488da69ff
Kai Huebl <kai@huebl-sgh.de>
2017-09-13 22:35:50 +0200

    added start script

---
+++ start.bat
@@ -0,0 +1,8 @@
+
+
+
+set PATH=C:\install\usr\lib;C:\local\boost_1_58_0\lib32-msvc-12.0;%PATH%
+C:\install\usr\bin\OpcUaServer3 CONSOLE C:\install\etc\OpcUaStack\ASNeG-Demo\OpcUaServer.xml
+
+
+pause
