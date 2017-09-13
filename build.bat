2d41f9af5d487bdfa2f8a294e12b6b8ed90968fe
Kai Huebl <kai@huebl-sgh.de>
2017-09-13 22:08:51 +0200

    added build directory

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ rmdir ./build
 cmake.exe -G"Visual Studio 12 2013" -DOPCUASTACK_INSTALL_PREFIX=C:\install -H./src/ -B./build 
 
 
-
+set DESTDIR=C:\install
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe ./build/INSTALL.vcxproj
 
 
