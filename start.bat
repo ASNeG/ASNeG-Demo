6ac491de2a329f6b73520bdf8682f6e6e022f234
Kai Huebl <kai@huebl-sgh.de>
2017-09-14 18:48:18 +0200

    added some comments to start script

--- start.bat
+++ start.bat
@@ -1,8 +1,15 @@
 
+@echo OFF
+REM
+REM added the following string to the Path environment variable
+REM
+REM Computer -> Systemeigenschaften -> Erweiterte Systemeinstallung -> Umgebungsvariablen
+REM
+REM Name:		Path
+REM String:		C:\install\usr\lib;C:\local\boost_1_58_0\lib32-msvc-12.0;C:\install\usr\bin
+REM
 
-
-set PATH=C:\install\usr\lib;C:\local\boost_1_58_0\lib32-msvc-12.0;%PATH%
-C:\install\usr\bin\OpcUaServer3 CONSOLE C:\install\etc\OpcUaStack\ASNeG-Demo\OpcUaServer.xml
+OpcUaServer3 CONSOLE C:\install\etc\OpcUaStack\ASNeG-Demo\OpcUaServer.xml
 
 
 pause
