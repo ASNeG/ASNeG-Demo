237157885ce56a410f87ec4fc80e81d5498d0e6b
Kai Huebl <kai@huebl-sgh.de>
2018-02-10 09:22:30 +0100

    added new class Authentication

---
+++ src/ASNeG-Demo/Library/Authentication.cpp
@@ -0,0 +1,60 @@
+/*
+   Copyright 2018 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#include "OpcUaStackCore/Base/os.h"
+#include "OpcUaStackCore/Base/Log.h"
+#include "ASNeG-Demo/Library/Authentication.h"
+
+namespace OpcUaServerApplicationDemo
+{
+
+	Authentication::Authentication(void)
+	: ioThread_(nullptr)
+	, applicationServiceIf_(nullptr)
+	, applicationInfo_(nullptr)
+	{
+	}
+
+	Authentication::~Authentication(void)
+	{
+	}
+
+	bool
+	Authentication::startup(
+		IOThread& ioThread,
+		ApplicationServiceIf& applicationServiceIf,
+		ApplicationInfo* applicationInfo
+	)
+	{
+		Log(Debug, "Authentication::startup");
+
+		ioThread_ = &ioThread;
+		applicationServiceIf_ = &applicationServiceIf;
+		applicationInfo_ = applicationInfo;
+
+		return true;
+	}
+
+	bool
+	Authentication::shutdown(void)
+	{
+		Log(Debug, "Authentication::shutdown");
+
+		return true;
+	}
+
+}
