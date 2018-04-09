182610ee758c0227e9e821c2d2713ced1d390f9b
Kai Huebl <kai@huebl-sgh.de>
2018-04-09 20:58:44 +0200

    added unittest to check crypt connection

---
+++ ftest/security/securityPolicyCrypt.py
@@ -0,0 +1,18 @@
+#!/usr/local/bion/python
+import sys, os
+import unittest
+
+from opcua import Client
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_basic128Rsa15(self):
+        client = Client("opc.tcp://127.0.0.1:8889")
+        client.set_security_string("Basic128Rsa15,SignAndEncrypt,certificate.der,private-key.pem")
+        client.connect()
+        client.disconnect()
+
+
+if __name__ == '__main__':
+    unittest.main()
+
