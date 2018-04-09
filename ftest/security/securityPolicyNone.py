c6870d5a786941fb906c8bac7f22694fe6f9f03b
Kai Huebl <kai@huebl-sgh.de>
2018-04-09 19:52:20 +0200

    added unittest securityPolicyNone.py

--- ftest/security/securityPolicyNone.py
+++ ftest/security/securityPolicyNone.py
@@ -1,5 +1,16 @@
 #!/usr/local/bion/python
 import sys, os
+import unittest
 
-print('Hello world...')
-exit(0)
+from opcua import Client
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_get_endpointr(self):
+        client = Client("opc.tcp://127.0.0.1:8889")
+        client.connect()
+        client.disconnect()
+
+if __name__ == '__main__':
+    unittest.main()
+
