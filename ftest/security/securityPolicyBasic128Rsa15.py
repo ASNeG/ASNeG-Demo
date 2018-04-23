8490c0758e0079a1a377ab242fa9bf92cb020069
Kai Huebl <kai@huebl-sgh.de>
2018-04-23 13:35:04 +0200

    added basic 128 rsa 15 encrytion test

---
+++ ftest/security/securityPolicyBasic128Rsa15.py
@@ -0,0 +1,23 @@
+#!/usr/local/bion/python
+import sys, os
+import unittest
+
+from opcua import Client
+
+import logging
+logging.basicConfig(format="%(levelname)s: %(message)s", level="DEBUG")
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_basic256(self):
+        client = Client("opc.tcp://" + os.environ['TEST_IP'] + ":" + os.environ['TEST_PORT'])
+        client.set_security_string("Basic128Rsa15,SignAndEncrypt,certificate.der,private-key.pem")
+        client.connect()
+        client.disconnect()
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
