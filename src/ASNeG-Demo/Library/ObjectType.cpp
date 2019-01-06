16aaa47df2a6db3585b2ff1c553c51d3b0d59b20
Kai Huebl <kai@huebl-sgh.de>
2019-01-06 20:54:42 +0100

    added new class ObjectType

---
+++ src/ASNeG-Demo/Library/ObjectType.cpp
@@ -0,0 +1,99 @@
+/*
+   Copyright 2019 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+
+#include "OpcUaStackCore/Base/os.h"
+#include "OpcUaStackCore/Base/Log.h"
+#include "OpcUaStackCore/Base/ConfigXml.h"
+#include "OpcUaStackCore/BuildInTypes/OpcUaIdentifier.h"
+#include "OpcUaStackCore/BuildInTypes/OpcUaExtensionObject.h"
+#include "OpcUaStackCore/StandardDataTypes/Range.h"
+#include "OpcUaStackCore/StandardDataTypes/EUInformation.h"
+#include "ASNeG-Demo/Library/ObjectType.h"
+#include "OpcUaStackServer/ServiceSetApplication/CreateObjectInstance.h"
+
+namespace OpcUaServerApplicationDemo
+{
+
+	ObjectType::ObjectType(void)
+	: ioThread_(nullptr)
+	, applicationServiceIf_(nullptr)
+	, applicationInfo_(nullptr)
+	{
+	}
+
+	ObjectType::~ObjectType(void)
+	{
+	}
+
+	bool
+	ObjectType::startup(
+		IOThread& ioThread,
+		ApplicationServiceIf& applicationServiceIf,
+		ApplicationInfo* applicationInfo
+	)
+	{
+		Log(Debug, "ObjectType::startup");
+
+		ioThread_ = &ioThread;
+		applicationServiceIf_ = &applicationServiceIf;
+		applicationInfo_ = applicationInfo;
+
+		// create object instance
+		if (!createObject()) {
+			Log(Error, "create variable error");
+			return false;
+		}
+
+		return true;
+	}
+
+	bool
+	ObjectType::shutdown(void)
+	{
+		Log(Debug, "ObjectType::shutdown");
+
+		return true;
+	}
+
+	bool
+	ObjectType::createObject(void)
+	{
+		//
+		// create a new object instance in the opc ua information model
+		//
+		Object::SPtr obj; // FIXME: todo
+		CreateObjectInstance createObjectInstance(
+			"http://ASNeG-Demo/ObjectType/",				// namespace name of the object instance
+			OpcUaLocalizedText("", "ObjectType"),			// display name of the object instance
+			OpcUaNodeId(85),								// parent node of the object instance
+			OpcUaNodeId(35),								// reference type between object and variable instance
+			obj
+		);
+
+		if (!createObjectInstance.query(applicationServiceIf_)) {
+			Log(Error, "create object response error");
+			return false;
+		}
+
+		// FIXME: todo
+
+		return true;
+	}
+
+
+}
---
+++ src/ASNeG-Demo/Library/ObjectType.h
@@ -0,0 +1,52 @@
+/*
+   Copyright 2019 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#ifndef __OpcUaServerApplicationDemo_ObjectType_h__
+#define __OpcUaServerApplicationDemo_ObjectType_h__
+
+#include "OpcUaStackCore/Utility/IOThread.h"
+#include "OpcUaStackCore/Application/ApplicationMethodContext.h"
+#include "OpcUaStackServer/Application/ApplicationIf.h"
+#include "OpcUaStackServer/Application/ApplicationInfo.h"
+#include "OpcUaStackServer/AddressSpaceModel/BaseNodeClass.h"
+
+using namespace OpcUaStackCore;
+using namespace OpcUaStackServer;
+
+namespace OpcUaServerApplicationDemo
+{
+
+	class ObjectType
+	{
+	  public:
+		ObjectType(void);
+		~ObjectType(void);
+
+		bool startup(IOThread& ioThread, ApplicationServiceIf& applicationServiceIf, ApplicationInfo* applicationInfo);
+		bool shutdown(void);
+
+	  private:
+		bool createObject(void);
+
+		IOThread* ioThread_;
+		ApplicationServiceIf* applicationServiceIf_;
+		ApplicationInfo* applicationInfo_;
+	};
+
+}
+
+#endif
